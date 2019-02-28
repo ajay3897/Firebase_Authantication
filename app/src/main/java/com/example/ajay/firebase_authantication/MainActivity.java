0a7745351c8d87e07d233ca5327b2a67f1182a49
Ajay Rathi <aajayrathi3897@gmail.com>
2019-02-28 19:16:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ajay/firebase_authantication/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.ajay.firebase_authantication;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    EditText ed1,ed2;
+    Button btn;
+    FirebaseAuth firebaseAuth;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        firebaseAuth=FirebaseAuth.getInstance();
+        progressDialog=new ProgressDialog(this);//this is use for seen proceing by internet or loading page from server
+        progressDialog.setMessage("Loading please wait.....");
+
+        ed1=findViewById(R.id.et_mail);
+        ed2=findViewById(R.id.et_pwd);
+        btn=findViewById(R.id.bt_reg);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();//login user
+
+            }
+        });
+    }
+    public  void  registerUser()
+    {
+        String nm= ed1.getText().toString().trim();//trim is use to remove spaces between the Strings
+        String passward=ed2.getText().toString().trim();
+        if(TextUtils.isEmpty(nm))
+        {
+            Toast.makeText(this,"email field is empty",Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(passward))
+        {
+            Toast.makeText(this,"passward field is empty",Toast.LENGTH_SHORT).show();
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(nm,passward).addOnCompleteListener(new OnCompleteListener<AuthResult>() {//sign in with emali
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful())
+                {
+                    progressDialog.dismiss();
+                    Toast.makeText(MainActivity.this,"successfully log in",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this,Home.class));
+                }
+                else
+                {
+                    progressDialog.dismiss();
+                    Toast.makeText(MainActivity.this,"some error 1",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
